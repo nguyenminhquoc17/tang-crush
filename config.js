8d6dcb83c77aeaff9838be8dfff41552fd83a5b7
nguyenminhquoc17 <86812624+nguyenminhquoc17@users.noreply.github.com>
2021-07-02 09:22:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Babi à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Em còn giận anh à?
+    tại em hư đok,
+    nên tui mới tuwk..
+    vô đây anh nói nè.`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: '520, 5620 babi :3 😙',
+    desc: 'Em có yêu anh hok:33 ',
+    btnYes: 'Cóooo nèeeee <33',
+    btnNo: 'Không, Anh bạn à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Nói cho anh lí do đi nèo <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Anh rất là yêu em <3333.',
